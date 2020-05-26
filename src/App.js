77a51daecbf76582d0a0f6d0aa4d099ce2784750
byohannes <42332389+byohannes@users.noreply.github.com>
2020-05-26 12:21:35 +0100

    updated components and all functionalities with out css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Scoreboard from './components/ScoreBoard';
+import allCountryScores from './components/Scores';
+import './App.css';
+
 const App = () => {
-  return <div>Hello</div>;
+  const [scoreOrder, setScoreOrder] = useState ('High');
+
+  const handleButtonClick = () => {
+    scoreOrder === 'High' ? setScoreOrder ('Low') : setScoreOrder ('High');
+  };
+
+  return (
+    <div className="App">
+      <h1>High Scores per Country</h1>
+      <button name="button" className="button" onClick={handleButtonClick}>
+        {' '}{scoreOrder}
+      </button>
+      {allCountryScores
+        .sort ((a, b) => a.name.localeCompare (b.name))
+        .map ((element, index) => (
+          <Scoreboard key={index} name={element.name} scores={element.scores} />
+        ))}
+    </div>
+  );
 };
+
 export default App;
